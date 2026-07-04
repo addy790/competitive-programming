91715246426bd2b23b8624a377b2df009ffbf3d3
addy790 <152245395+addy790@users.noreply.github.com>
2026-07-04 17:50:52 +0530

    Solve Codeforces 4A - Watermelon

---
+++ codeforces/800/1A_Theatresquare.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    using ll = long long;
+    ll n, m, a;
+    cin>>n>>m>>a;
+    ll len = (n+a-1)/a;
+    ll wid = (m+a-1)/a;
+    ll ans = len*wid;
+    cout<<ans<<endl;
+    return 0;
+}
